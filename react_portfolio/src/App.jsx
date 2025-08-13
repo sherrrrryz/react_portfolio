e4dd6a0867d0cc1fa634e64aa0a058b55d68c3f4
axue <sherrrryz@outlook.com>
2025-08-13 16:30:56 -0400

    new_file

--- react_portfolio/src/App.jsx
+++ react_portfolio/src/App.jsx
@@ -1,10 +1,19 @@
 
-import './App.css'
+import './App.css';
+import React from 'react';
+import Home from './Pages/Home/Homescreen';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
 
   return (
-    <div>Hello</div>
+    <div>
+      <Home />
+    </div>
   )
 }
 
